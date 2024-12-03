62f70e552fde34cfdebde26bab3fa170e0ae706a
Steven Kotwal <7886355+panda@users.noreply.github.com>
2024-12-03 16:29:23 -0500

    Filter results & implement subgraph

--- app.py
+++ app.py
@@ -27,20 +27,19 @@ def normalize_scores(scores):
 @app.route("/search", methods=["GET"])
 def search():
     number_of_results = int(request.args.get("number_of_results", 10))
-    salsa_weight = request.args.get("salsa", 0)
-    hits_weight = request.args.get("hits", 1)
-    hits_hub_weight = request.args.get("hits_hub", 1)
-    hits_authority_weight = request.args.get("hits_authority", 1)
-    pagerank_weight = request.args.get("pagerank", 1)
-    eigenvector_weight = request.args.get("eigenvector", 0)
-    semantic_similarity_weight = request.args.get("semantic_similarity", 0)
-    publish_date_weight = request.args.get("publish_date", 0)
+    salsa_weight = float(request.args.get("salsa", 0))
+    hits_weight = float(request.args.get("hits", 1))
+    hits_hub_weight = float(request.args.get("hits_hub", 1))
+    hits_authority_weight = float(request.args.get("hits_authority", 1))
+    pagerank_weight = float(request.args.get("pagerank", 1))
+    eigenvector_weight = float(request.args.get("eigenvector", 0))
+    semantic_similarity_weight = float(request.args.get("semantic_similarity", 0))
+    publish_date_weight = float(request.args.get("publish_date", 0))
 
-    hits_weight = hits_weight / 2
-    hits_hub_weight = hits_hub_weight / 2
-    hits_authority_weight = hits_authority_weight / 2
-
-    pagerank_weight = pagerank_weight / 2
+    hits_weight /= 2
+    hits_hub_weight /= 2
+    hits_authority_weight /= 2
+    pagerank_weight /= 2
 
     hits_hub_scores, hits_authority_scores = nx.hits(citation_graph)
     pagerank_scores = nx.pagerank(citation_graph)
@@ -49,7 +48,6 @@ def search():
     hits_authority_scores = normalize_scores(hits_authority_scores)
     pagerank_scores = normalize_scores(pagerank_scores)
 
-    # O(n) time complexity
     overall_scores = {}
     for node in citation_graph.nodes():
         overall_scores[node] = (
@@ -61,28 +59,30 @@ def search():
         ) + (pagerank_weight * pagerank_scores[node])
     overall_scores = normalize_scores(overall_scores)
 
-    # O(n logn) time complexity
     top_papers = sorted(overall_scores.items(), key=lambda x: x[1], reverse=True)[
         :number_of_results
     ]
 
     response = []
-    for paper in top_papers:
-        node = paper[0]
-        data = citation_graph.nodes[node]
-        print(data)
+    top_paper_ids = []
+    for paper_id, score in top_papers:
+        data = citation_graph.nodes[paper_id]
         response.append(
             {
-                "paper_id": node,
+                "paper_id": paper_id,
                 "title": data.get("title", ""),
                 "authors": data.get("authors", ""),
                 "year": data.get("year", ""),
-                "overall_score": paper[1],
+                "overall_score": score,
             }
         )
+        top_paper_ids.append(paper_id)
 
-    return jsonify({"success": True, "results": response})
-    
+    subgraph = citation_graph.subgraph(top_paper_ids)
+    subgraph_data = nx.readwrite.json_graph.node_link_data(subgraph)
+
+    return jsonify({"success": True, "results": response, "subgraph": subgraph_data})
+
 
 def pagerank_helper(graph, alpha=0.85, max_iter=100, tol=1e-6):
     nodes = list(graph.nodes())
